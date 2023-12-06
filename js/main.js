0b1011fe416b7969495f24edc6066e48c3ee987b
aniaso <lolinnlol@gmail.com>
2023-12-06 15:02:16 +0200

    e2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
   const header = document.querySelector(".header");
   window.onscroll = () => {
-    if (window.pageYOffset > 50) {
+    if (window.scrollY > 50) {
       header.classList.add("header_active");
     } else {
       header.classList.remove("header_active");
@@ -38,7 +38,7 @@
     const headerElHeight = document.querySelector(".header").clientHeight;
     let target = document.querySelector(targetEl);
     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-    let startPosition = window.pageYOffset;
+    let startPosition = window.scrollY;
     let startTime = null;
 
     const ease = function (t, b, c, d) {
